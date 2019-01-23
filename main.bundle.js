0a2cc012b7cece769dc885aa895e2a1f33a29d16
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-23 13:44:21 -0700

    Add location search field and format

--- main.bundle.js
+++ main.bundle.js
@@ -44,8 +44,37 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	// This file is in the entry point in your webpack config.
 	"use strict";
+
+	function fullLogOut() {
+	  setSweaterDisplay();
+	  setLogOut();
+	}
+
+	function fullLogIn() {
+	  rotateImage();
+	  document.getElementById("user").innerHTML = "<li id='user' class='nav'>Welcome, bdiveley! <button id='logout-btn' class='nav' onclick='fullLogOut()'>Log Out</button></li>";
+	}
+
+	function setLogOut() {
+	  document.getElementById("user").innerHTML = "<li id='user' class='nav'>Welcome! <button id='logout-btn' class='nav' onclick='fullLogIn()'>Log In</button></li>";
+	}
+
+	function setSweaterDisplay() {
+	  document.getElementById("display").innerHTML = "<img id='display' class='sweater' src='gray-sweater.png' alt='Sweater'>";
+	}
+
+	function rotateImage() {
+	  $('.sweater').animate({ transform: 360 }, {
+	    step: function step(now, fx) {
+	      $(this).css({
+	        '-webkit-transform': 'rotate(' + now + 'deg)',
+	        '-moz-transform': 'rotate(' + now + 'deg)',
+	        'transform': 'rotate(' + now + 'deg)'
+	      });
+	    }
+	  });
+	}
 
 /***/ })
 /******/ ]);
